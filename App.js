223cffa413ad2badbcffa13e10cdc4a3ec6b53ea
PriyankaBharti-2909 <92639823+PriyankaBharti-2909@users.noreply.github.com>
2023-05-30 12:52:34 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,19 @@
+
+import React from 'react';
+import './App.css';
+import Calculator from './cal';
+
+
+function App() {
+  return (
+    <div>
+      <h1><center>React Calculator</center></h1>
+      <Calculator />
+    </div>
+  );
+};
+
+export default App;
+
+          
+
